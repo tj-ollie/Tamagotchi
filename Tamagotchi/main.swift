446cab0d99c2849beb246a20160f25a5bcce1b03
Ollie <taylor-johnson.o@etoncollege.org.uk>
2023-11-24 17:10:00 +0000

    PRINTED HELLO :)

--- Tamagotchi/main.swift
+++ Tamagotchi/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+var age = 0
+var weight = 0
+var hungerMetre = 100 // 0 is hungriest
+var naughtyMetre = 50 // 0 is naughtiest
+var moodMetre = 100 // 0 is happiest
+var isSick = false
+var isAlive = true
+var gameLoop = true
 
+print("Hello")
+print("Welcome to your TJGotchi")
+
+while gameLoop == true {
+
+}
